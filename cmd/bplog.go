6fa617b51fb16f8536e550f9a5e85254163d2954
James Scott <scottjab@gmail.com>
2024-09-26 14:54:04 -0700

    Add a cli flag to import data to db

--- cmd/bplog.go
+++ cmd/bplog.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/scottjab/bplog"
+import (
+	"flag"
+	"github.com/scottjab/bplog"
+	"log"
+)
 
 func main() {
+	loadData := flag.String("load-data", "", "load data into db.")
+	flag.Parse()
+
 	b := bplog.NewBPLog()
 	b.DBMigrate()
-	//b.LoadCSVIntoDB("/Users/scottjab/Downloads/history.csv")
+
+	if *loadData != "" {
+		log.Printf("loading data from %s", *loadData)
+		b.LoadCSVIntoDB(*loadData)
+	}
 	b.ServeHttp()
+}
+
+func loadData(string) {
+
 }
