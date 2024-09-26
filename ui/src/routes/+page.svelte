9a1ddccb89276212915f8ed9f4937468b17da640
James Scott <scottjab@gmail.com>
2024-09-26 08:07:21 -0700

    Add sevelt static builds

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let name = 'Jim'
+</script>
+
+
+<h1>Welcome to {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
