e0dcc0e5adfa35aa5a49adea6190fac13b2f1dad
James Scott <scottjab@gmail.com>
2024-09-26 09:02:46 -0700

    Add BP route

--- webservice.go
+++ webservice.go
@@ -15,6 +15,49 @@ func (b *BPLog) ServeHttp() {
 			"time_stamp": time.Now(),
 		})
 	})
+	r.GET("/api/bp", b.ServeBPData)
 	r.NoRoute(gin.WrapH(http.FileServer(http.Dir("ui/build"))))
 	r.Run()
+}
+
+type BPChartData struct {
+	Labels   []string  `json:"labels"`
+	Datasets []DataSet `json:"datasets"`
+}
+type DataSet struct {
+	Name      string `json:"name"`
+	ChartType string `json:"chartType"`
+	Values    []int  `json:"values"`
+}
+
+func (b *BPLog) ServeBPData(ctx *gin.Context) {
+	readings := []BloodPressure{}
+	r := b.db.Find(&readings)
+	chart := BPChartData{
+		Labels: make([]string, r.RowsAffected),
+	}
+	systolic := DataSet{
+		Name:      "Systolic",
+		ChartType: "line",
+		Values:    make([]int, r.RowsAffected),
+	}
+
+	diastolic := DataSet{
+		Name:      "Diastolic",
+		ChartType: "line",
+		Values:    make([]int, r.RowsAffected),
+	}
+	pulse := DataSet{
+		Name:      "Pulse",
+		ChartType: "line",
+		Values:    make([]int, r.RowsAffected),
+	}
+	for i, reading := range readings {
+		chart.Labels[i] = reading.Date.String()
+		systolic.Values[i] = reading.Systolic
+		diastolic.Values[i] = reading.Diastolic
+		pulse.Values[i] = reading.Pulse
+	}
+	chart.Datasets = []DataSet{systolic, diastolic, pulse}
+	ctx.JSON(http.StatusOK, chart)
 }
